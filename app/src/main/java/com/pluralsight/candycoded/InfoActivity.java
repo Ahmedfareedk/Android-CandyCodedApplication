6003a3d86536885796b5fa7ee574fe2bdff0751d
Ahmedfareedk <ahmedfareed273@gmail.com>
2020-10-06 10:58:07 +0200

    Share Data Intent Explicit Intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -46,4 +47,5 @@ public class InfoActivity extends AppCompatActivity {
         phoneIntent.setData(Uri.parse("tel:0123456789"));
         startActivity(phoneIntent);
     }
+
 }
